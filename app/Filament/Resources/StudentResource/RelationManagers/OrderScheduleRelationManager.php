cfb6352eb7df97f4ed5a3236a0d8040fcea6fe39
Aegon017 <kondanagamalleswararao016@gmail.com>
2024-11-25 10:16:06 +0000

    colspnafull batch select

--- app/Filament/Resources/StudentResource/RelationManagers/OrderScheduleRelationManager.php
+++ app/Filament/Resources/StudentResource/RelationManagers/OrderScheduleRelationManager.php
@@ -30,6 +30,7 @@ class OrderScheduleRelationManager extends RelationManager
                         })
                     )
                     ->searchable()
+                    ->columnSpanFull()
             ]);
     }
 
