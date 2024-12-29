430a76b0de3cbf71c483fa1691da8dc2c2385eac
Aegon017 <kondanagamalleswararao016@gmail.com>
2024-12-29 18:45:53 +0000

    changes in slider migration and the home feature card icon

--- database/migrations/2024_11_14_235347_create_sliders_table.php
+++ database/migrations/2024_11_14_235347_create_sliders_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('sliders', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('content');
+            $table->text('content');
             $table->string('image');
             $table->string('button_name');
             $table->string('redirect_url');
