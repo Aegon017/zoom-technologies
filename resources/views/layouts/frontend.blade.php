a2214ddea1864f0ab2b120f97960a75f59744774
Aegon017 <kondanagamalleswararao016@gmail.com>
2024-12-31 05:23:18 +0000

    schema issue resolved in the fronted blade

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -58,11 +58,13 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         rel="stylesheet">
-    @if ((string) $localSchema)
-        <script type="application/ld+json">{{ $localSchema }}</script>
-    @endif
-    @if ((string) $organizationSchema)
-        <script type="application/ld+json">{{ $organizationSchema }}</script>
+    @if ($localSchema & $organizationSchema)
+        @if ((string) $localSchema)
+            <script type="application/ld+json">{{ $localSchema }}</script>
+        @endif
+        @if ((string) $organizationSchema)
+            <script type="application/ld+json">{{ $organizationSchema }}</script>
+        @endif
     @endif
     @livewireStyles
 </head>
