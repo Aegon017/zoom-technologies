192ad4f2b2c16a7358b8ab5ac8f42488df17d088
Aegon017 <kondanagamalleswararao016@gmail.com>
2025-01-28 11:46:58 +0000

    removed course courdinators

--- app/Filament/Resources/CouponResource.php
+++ app/Filament/Resources/CouponResource.php
@@ -25,7 +25,7 @@ class CouponResource extends Resource
 {
     protected static ?string $model = Coupon::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-ticket';
 
     public static function form(Form $form): Form
     {
