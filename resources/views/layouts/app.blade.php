238ef7e313d4466e3414846bb2f00180f47c8f61
Aegon017 <kondanagamalleswararao016@gmail.com>
2025-02-03 09:45:09 +0000

    font awsome 6 in app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="{{ asset('frontend/assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
     <link rel="stylesheet" href="{{ asset('frontend/assets/css/fontawesome-all.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/assets/css/flaticon.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/assets/css/animate.css') }}">
