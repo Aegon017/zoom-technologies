828cc73694118c1695b830ae382a0b8f3fd5f1b8
Aegon017 <kondanagamalleswararao016@gmail.com>
2025-02-19 12:16:47 +0000

    widget permission also coompleted

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -3,13 +3,17 @@
 namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
+use App\Filament\Widgets\OrdersOverview;
 use Filament\Actions;
+use Filament\Pages\Concerns\ExposesTableToWidgets;
 use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 use Illuminate\Database\Eloquent\Builder;
 
 class ListOrders extends ListRecords
 {
+    use ExposesTableToWidgets;
+
     protected static string $resource = OrderResource::class;
 
     protected function getHeaderActions(): array
@@ -32,5 +36,12 @@ class ListOrders extends ListRecords
     public function getDefaultActiveTab(): string|int|null
     {
         return 'success';
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            OrdersOverview::class,
+        ];
     }
 }
