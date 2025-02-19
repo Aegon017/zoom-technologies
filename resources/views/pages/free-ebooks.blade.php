4c453dc1f1b7249ab46965cbf0caec511bd0892a
Aegon017 <kondanagamalleswararao016@gmail.com>
2025-02-19 14:10:18 +0000

    added popup for freebooks

--- resources/views/pages/free-ebooks.blade.php
+++ resources/views/pages/free-ebooks.blade.php
@@ -1,4 +1,15 @@
 <x-frontend-layout>
+    <style>
+        @media (min-width: 992px) {
+            .modal-lg {
+                max-width: 100% !important;
+            }
+
+            .modal {
+                overflow: hidden !important;
+            }
+        }
+    </style>
     @if ($metaDetail != null)
         <x-slot:metaTitle>
             {{ $metaDetail->title }}
@@ -37,8 +48,8 @@
                                     @foreach ($materials as $material)
                                         <div class="col-lg-3 col-md-4 col-sm-2 col-xs-12 mb-4">
                                             <div class="study-material-item">
-                                                <a href="{{ $material->material_pdf ? asset(Storage::url($material->material_pdf)) : $material->material_url }}"
-                                                    target="_blank" rel="noopener noreferrer">
+                                                <a href="javascript:void(0);" class="open-iframe-modal"
+                                                    data-url="{{ $material->material_pdf ? asset(Storage::url($material->material_pdf)) : $material->material_url }}">
                                                     <img src="{{ asset(Storage::url($material->image)) }}"
                                                         alt="{{ $material->image_alt }}">
                                                     <div class="study-material-content">
@@ -57,4 +68,40 @@
         </section>
     @endif
     <x-related-courses />
+    <!-- Modal Structure -->
+    <div class="modal fade px-4" id="iframeModal" tabindex="-1" role="dialog" aria-labelledby="iframeModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog modal-lg w-100" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <iframe id="iframeContent" src=""
+                        style="width: 100%; height: 80dvh; border: none;"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Include jQuery and Bootstrap JS (if not already included) -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('.open-iframe-modal').on('click', function() {
+                var url = $(this).data('url');
+                $('#iframeContent').attr('src', url);
+                $('#iframeModal').modal('show');
+            });
+
+            // Clear the iframe source when the modal is closed
+            $('#iframeModal').on('hidden.bs.modal', function() {
+                $('#iframeContent').attr('src', '');
+            });
+        });
+    </script>
 </x-frontend-layout>
